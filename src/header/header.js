0011d3ac24bb4ec7f7bdc8edde2f02d698d7e426
zhukouskayat <64001084+zhukouskayat@users.noreply.github.com>
2021-01-23 18:24:55 +0300

    Added FontAwesome

--- src/header/header.js
+++ src/header/header.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import logo from '../ill/logo.svg';
 import './header.css'
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => (
   <div id="header">
   <a href="index.html"><img className="logo" src={logo} alt="logo" /></a>
   <a href="languageCourses.html" className="courses">Language courses</a>
   <a href="onlineEducation.html" className="contact">Contact us</a>
-  <a href="signUp.html" className="signUp">Sign up<i className="fas fa-user"></i></a>
+  <a href="signUp.html" className="signUp">
+  Sign up
+  <FontAwesomeIcon icon={faUser} />
+  </a>
 </div>
 )
 
