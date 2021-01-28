444a69dbdd6578c38811208cfec8f352cb7a51d2
zhukouskayat <64001084+zhukouskayat@users.noreply.github.com>
2021-01-28 23:51:33 +0300

    Updated banner

--- src/content/main/main.js
+++ src/content/main/main.js
@@ -3,7 +3,6 @@ import React from "react";
 import "./main.css";
 
 import Banner from "./banner/banner";
-import BannerBckg from "./banner/bannerBackground";
 import InfoBlock from "./infoBlock/infoBlock";
 import RequestCallBack from "./request/request";
 
@@ -11,7 +10,6 @@ import RequestCallBack from "./request/request";
 const Main = () => (
   <React.Fragment>
     <Banner />
-    <BannerBckg />
     <InfoBlock />
     <RequestCallBack />
   </React.Fragment>
