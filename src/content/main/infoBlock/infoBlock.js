8245a68889d55ef81e1883e42553ab67a4e57eb8
zhukouskayat <64001084+zhukouskayat@users.noreply.github.com>
2021-01-29 14:02:54 +0300

    Update styles

--- src/content/main/infoBlock/infoBlock.js
+++ src/content/main/infoBlock/infoBlock.js
@@ -14,10 +14,7 @@ const InfoBlock = () => (
       native speakers while exploring local culture and lifestyle. You will
       begin to feel comfortable using English in casual situations and your
       comprehension will grow by leaps and bounds as you learn to use the
-      language more confidently. There are English courses suited to all ages,
-      purposes and proficiency levels, so you will definitely find the option
-      that is perfect for you. Specialists at LinguaLand are always happy to
-      help you choose the best options and prepare for this unforgettable trip.
+      language more confidently.
     </p>
   </div>
 );
