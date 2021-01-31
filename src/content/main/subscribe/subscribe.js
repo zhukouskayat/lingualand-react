cc882b5e6517c590cd02c67219f7b637acb20cef
zhukouskayat <64001084+zhukouskayat@users.noreply.github.com>
2021-01-31 23:39:03 +0300

    Added form validation (subscription)

--- src/content/main/subscribe/subscribe.js
+++ src/content/main/subscribe/subscribe.js
@@ -4,30 +4,65 @@ import "./subscribe.css";
 
 import Popup from "../popup/popup";
 
+import subIll from "../../../ill/subscribe.svg";
+
 const Subscribe = () => {
   const [modalActive, setModalActive] = useState(false);
-  const [text, setText] = useState("");
+  const [email, setEmail] = useState("");
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [emailError, setEmailError] = useState("Email cannot be empty");
+  const [emailValid, setEmailValid] = useState(false);
 
-  function handleSubmit(event){
-    event.preventDefault();
-
-    const emailValue = text;
-  
-    if (emailValue !== "") {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (emailError !== "") {
+      emailValid();
+    } else {
+      setEmailValid(true);
+      setEmail("");
       setModalActive(true);
-      setText("")
-    } 
+    }
   }
+
+  const emailHandler = (e) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(e.target.value).toLowerCase())) {
+      setEmailError("Email is not valid");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const blurHandler = (e) => {
+    if (e.target.name === "email") {
+      setEmailDirty(true);
+    }
+  };
+
+  const popupSub = () => {
+    return <h3 className="popupSub">Thanks for your subscription!</h3>;
+  };
 
   return (
     <div className="subscribe">
       <h2>Subscribe!</h2>
       <form>
-        <input type="email" placeholder="Email..." className="emailSub" value={text} onChange={(event) => setText(event.target.value)}/>
+        <input
+          onChange={(e) => emailHandler(e)}
+          onBlur={(e) => blurHandler(e)}
+          value={email}
+          name="email"
+          type="email"
+          placeholder="Email..."
+          className="emailSub"
+        />
+        {emailDirty && emailError && <div>{emailError}</div>}
         <div>
-        <button type="submit" className="buttonSub" onClick={(event) => handleSubmit(event)}>
-          Subscribe
-        </button>
+          <button disabled={emailValid} onClick={(e) => handleSubmit(e)} type="submit" className="buttonSub">
+            Subscribe
+          </button>
         </div>
       </form>
       <Popup
@@ -36,13 +71,8 @@ const Subscribe = () => {
         children={popupSub()}
         name={"Canсel"}
       />
+      <img src={subIll} alt="illustration" className="subIll"></img>
     </div>
-  );
-};
-
-const popupSub = () => {
-  return (
-      <h3 className="popupSub" >Thanks for your subscription!</h3>
   );
 };
 
