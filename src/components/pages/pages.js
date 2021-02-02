4e174080835eba85db971489f3a19eec8a56d34d
zhukouskayat <64001084+zhukouskayat@users.noreply.github.com>
2021-02-02 14:27:49 +0300

    Fixed routing

--- src/components/pages/pages.js
+++ src/components/pages/pages.js
@@ -7,11 +7,12 @@ import Courses from "components/pages/courses/courses";
 import ContactUs from "components/pages/contactUs/contactUs";
 import SignUp from "components/pages/signUp/signUp";
 
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 const Pages = () => (
   <div id="pages">
-    <Route exact path="/">
+  <Switch>
+  <Route exact path="/">
       <Main />
     </Route>
     <Route path="/courses">
@@ -23,6 +24,7 @@ const Pages = () => (
     <Route path="/login">
       <SignUp />
     </Route>
+  </Switch>
   </div>
 );
 
