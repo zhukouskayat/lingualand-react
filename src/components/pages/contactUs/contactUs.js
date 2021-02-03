cfbed2120220c9ae9ef49bba12a1cf4419b5d7d1
zhukouskayat <64001084+zhukouskayat@users.noreply.github.com>
2021-02-03 11:21:21 +0300

    Added page contactUS

--- src/components/pages/contactUs/contactUs.js
+++ src/components/pages/contactUs/contactUs.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 import "./contactUs.css";
+import PhoneNumbers from "./phoneNumbers/phoneNumbers" 
 
 const ContactUs = () => (
-<div>
-  Contact Us
-</div>
+  <div id="contacts">
+    <h2>Contacts</h2>
+    <h3>Customer support phone numbers!</h3>
+    <PhoneNumbers />
+    <h3>United States</h3>
+    <p>LinguaLand Inc. registered in the USA.</p>
+    <p>Our head office is located at:</p>
+    <p>
+      Suite 120 and 200, 611 Gateway Blvd, South San Francisco, CA 94080, USA
+    </p>
+  </div>
 );
 
 export default ContactUs;
---
+++ src/components/pages/contactUs/phoneNumbers/phoneNumbers.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./phoneNumbers.css";
+
+const PhoneNumbers = () => (
+  <div className="phoneNumbers">
+    <div>
+      <p>USA</p>
+      <p>+1 415 349 00 63</p>
+    </div>
+    <div>
+      <p>Spain</p>
+      <p>+34 932 20 0797</p>
+    </div>
+    <div>
+      <p>Malta</p>
+      <p>+356 2124 1234</p>
+    </div>
+    <div>
+      <p>Germany</p>
+      <p>+4930 229 11 29</p>
+    </div>
+    <div>
+      <p>UK</p>
+      <p>+44 0 20 7229 72 81</p>
+    </div>
+  </div>
+);
+
+export default PhoneNumbers;
